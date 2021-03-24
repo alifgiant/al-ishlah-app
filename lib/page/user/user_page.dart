427e184610748c3435f4907ee83b51e04d49077a
alifgiant <alif.akbar@bukalapak.com>
2021-03-24 16:13:08 +0700

    all layout of setting screen

--- lib/page/user/user_page.dart
+++ lib/page/user/user_page.dart
@@ -24,11 +24,6 @@ class _UserPageState extends State<UserPage> {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Akun Kamu'),
-        leading: CupertinoButton(
-          child: Text('Log Out'),
-          padding: EdgeInsets.zero,
-          onPressed: () {},
-        ),
         trailing: CupertinoButton(
           child: Text('Edit'),
           padding: EdgeInsets.zero,
@@ -38,23 +33,130 @@ class _UserPageState extends State<UserPage> {
       child: Material(
         child: ListView(
           children: [
-            // Row(
-            //   children: [
-            //     // CircleAvatar(
-            //     //   child: Icon(CupertinoIcons.iconFontPackage),
-            //     // ),
-            //   ],
-            // ),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(CupertinoIcons.person),
-              ),
-              title: Text('Muhammad Alif'),
-            ),
-            Center(child: Text('User')),
+            getProfilePictureSection(),
+            Container(height: 26),
+            getAccountFormSection(context),
+            Container(height: 26),
+            getAboutAppSection(context),
+            Container(height: 26),
+            getLogoutButton(),
           ],
         ),
       ),
+    );
+  }
+
+  Padding getLogoutButton() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: CupertinoButton(
+        child: Text('Log Out'),
+        padding: EdgeInsets.zero,
+        color: CupertinoColors.destructiveRed,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget getAboutAppSection(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Tentang Aplikasi', style: TextStyle(fontSize: 18)),
+              Text(
+                'v0.1',
+                style: CupertinoTheme.of(context).textTheme.textStyle,
+              ),
+            ],
+          ),
+          Container(height: 6),
+          ListTile(
+            title: Text('FAQ'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(color: CupertinoColors.secondaryLabel),
+            ),
+            onTap: () {},
+            tileColor: CupertinoColors.white,
+            dense: true,
+          ),
+          Container(height: 6),
+          ListTile(
+            title: Text('Kebijakan Layanan'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(color: CupertinoColors.secondaryLabel),
+            ),
+            onTap: () {},
+            tileColor: CupertinoColors.white,
+            dense: true,
+          ),
+          Container(height: 6),
+          ListTile(
+            title: Text('Hubungi Kami'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(color: CupertinoColors.secondaryLabel),
+            ),
+            onTap: () {},
+            tileColor: CupertinoColors.white,
+            dense: true,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getAccountFormSection(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Email',
+            style: TextStyle(fontSize: 16),
+          ),
+          Container(height: 6),
+          CupertinoTextField(
+            placeholder: 'Email',
+            padding: const EdgeInsets.all(12),
+          ),
+          Container(height: 12),
+          Text(
+            'Phone Number',
+            style: TextStyle(fontSize: 16),
+          ),
+          Container(height: 6),
+          CupertinoTextField(
+            placeholder: 'Phone Number',
+            padding: const EdgeInsets.all(12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column getProfilePictureSection() {
+    return Column(
+      children: [
+        Container(height: 6),
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: CircleAvatar(
+            child: Icon(CupertinoIcons.person, size: 42),
+            radius: 42,
+          ),
+        ),
+        Text(
+          'Muhammad Alif Akbar',
+          style: TextStyle(fontSize: 26),
+        ),
+      ],
     );
   }
 }
